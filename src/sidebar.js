0a9e803d4e0dbc8936d436860da2a17881a18e17
Keith R. Potempa <10837115+keithrpotempa@users.noreply.github.com>
2021-03-06 09:39:23 -0600

    removed jquery to lock background, some filler text

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,20 +1,6 @@
 import $ from "jquery";
 
 const hookupAccountSidebar = () => {
-  // If the sidebar is not open, then it will open;
-  // so lock the body's scrolling
-  $("#accountSidebarOpenButton").click(() => {
-    if (!$("#accountSidebarCloseButton").hasClass("show")) {
-      lockBodyScroll();
-    }
-  });
-
-  // When the account sidebar is closed,
-  // unlock the body's scrolling
-  $("#accountSidebarCloseButton").click(() => {
-    unlockBodyScroll();
-  });
-
   // If you click away from the sidebar,
   // while it is open, it collapses
   $(document.body).click((e) => {
@@ -23,17 +9,8 @@ const hookupAccountSidebar = () => {
       $("#accountSidebar").hasClass("show")
     ) {
       $("#accountSidebar").collapse("hide");
-      unlockBodyScroll();
     }
   });
-};
-
-const lockBodyScroll = () => {
-  $("body").addClass("scroll-lock");
-};
-
-const unlockBodyScroll = () => {
-  $("body").removeClass("scroll-lock");
 };
 
 export default hookupAccountSidebar;
