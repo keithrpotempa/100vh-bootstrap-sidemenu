6ece7231d115aa92b5817379d5a2a7cb76c70810
Keith R. Potempa <10837115+keithrpotempa@users.noreply.github.com>
2021-03-10 16:59:56 -0600

    Added scroll-locking and remembering vertical positioning, reworked body and CSS classes

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,16 +1,44 @@
 import $ from "jquery";
 
 const hookupAccountSidebar = () => {
+  // If the sidebar is not open, then it will open;
+  // so lock the body's scrolling
+  $(".sidebar-open-button").click(() => {
+    if (!$(".sidebar-open-button").hasClass("show")) {
+      lockBodyScroll();
+      $("#sidebar").collapse("show");
+    }
+  });
+
+  // When the account sidebar is closed,
+  // unlock the body's scrolling
+  $(".sidebar-close-button").click(() => {
+    unlockBodyScroll();
+  });
+
   // If you click away from the sidebar,
   // while it is open, it collapses
   $(document.body).click((e) => {
     if (
-      !$(e.target).closest("#accountSidebar").length &&
-      $("#accountSidebar").hasClass("show")
+      !$(e.target).closest("#sidebar").length &&
+      $("#sidebar").hasClass("show")
     ) {
-      $("#accountSidebar").collapse("hide");
+      $("#sidebar").collapse("hide");
+      unlockBodyScroll();
     }
   });
+};
+
+const lockBodyScroll = () => {
+  document.body.style.top = `-${window.scrollY}px`;
+  $(document.body).addClass("scroll-lock");
+};
+
+const unlockBodyScroll = () => {
+  const scrollY = document.body.style.top;
+  $(document.body).removeClass("scroll-lock");
+  document.body.style.top = "";
+  window.scrollTo(0, parseInt(scrollY || "0") * -1);
 };
 
 export default hookupAccountSidebar;
